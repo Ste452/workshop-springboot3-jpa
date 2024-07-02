c9a3df9de9d56513280889351ce5517d36ace1b4
Steve <falecomstevevaz@gmail.com>
2024-07-02 20:41:07 -0300

    First commit

---
+++ src/test/java/com/steve/applications/demo_example/DemoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.applications.demo_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

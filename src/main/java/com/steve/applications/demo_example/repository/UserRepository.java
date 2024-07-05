787c0cd1fb63937c1d4cff797d9ea5bcfc06c091
Steve <falecomstevevaz@gmail.com>
2024-07-05 19:23:49 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/steve/applications/demo_example/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.steve.applications.demo_example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.steve.applications.demo_example.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+}
